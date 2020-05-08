59d29e0fa10795fbbc542a7a541e516256430a25
Martin <7252614+Lhoerion@users.noreply.github.com>
2020-05-08 22:07:55 +0200

    feat: Add types-webview subpackage

---
+++ webview/index.d.ts
@@ -0,0 +1,33 @@
+export {};
+declare global {
+  interface Alt {
+    /**
+     * Emits specified event across particular client.
+     *
+     * @param eventName Name of the event.
+     * @param args Rest parameters for emit to send.
+     */
+    emit(eventName: string, ...args: any[]): void;
+
+    /**
+     * Unsubscribes from client event handler with specified listener.
+     *
+     * @remarks Listener should be of the same reference as when event was subscribed.
+     * @param eventName Name of the event.
+     * @param listener Listener that should be removed.
+     */
+    off(eventName: string, listener: (...args: any[]) => void): void;
+
+    /**
+     * Subscribes to client event handler with specified listener.
+     *
+     * @param eventName Name of the event.
+     * @param listener Listener that should be added.
+     */
+    on(eventName: string, listener: (...args: any[]) => void): void;
+  }
+
+  interface Window {
+    alt: Alt;
+  }
+}
