a06ed818afacc0ce79e3d4649937dbcdaf43d061
Martin <7252614+Lhoerion@users.noreply.github.com>
2020-05-19 18:54:21 +0200

    fix: Expose WebView global variable

--- webview/index.d.ts
+++ webview/index.d.ts
@@ -1,33 +1,32 @@
-export {};
-declare global {
-  interface Alt {
-    /**
-     * Emits specified event across particular client.
-     *
-     * @param eventName Name of the event.
-     * @param args Rest parameters for emit to send.
-     */
-    emit(eventName: string, ...args: any[]): void;
+interface Alt {
+  /**
+   * Emits specified event across particular client.
+   *
+   * @param eventName Name of the event.
+   * @param args Rest parameters for emit to send.
+   */
+  emit(eventName: string, ...args: any[]): void;
 
-    /**
-     * Unsubscribes from client event handler with specified listener.
-     *
-     * @remarks Listener should be of the same reference as when event was subscribed.
-     * @param eventName Name of the event.
-     * @param listener Listener that should be removed.
-     */
-    off(eventName: string, listener: (...args: any[]) => void): void;
+  /**
+   * Unsubscribes from client event handler with specified listener.
+   *
+   * @remarks Listener should be of the same reference as when event was subscribed.
+   * @param eventName Name of the event.
+   * @param listener Listener that should be removed.
+   */
+  off(eventName: string, listener: (...args: any[]) => void): void;
 
-    /**
-     * Subscribes to client event handler with specified listener.
-     *
-     * @param eventName Name of the event.
-     * @param listener Listener that should be added.
-     */
-    on(eventName: string, listener: (...args: any[]) => void): void;
-  }
-
-  interface Window {
-    alt: Alt;
-  }
+  /**
+   * Subscribes to client event handler with specified listener.
+   *
+   * @param eventName Name of the event.
+   * @param listener Listener that should be added.
+   */
+  on(eventName: string, listener: (...args: any[]) => void): void;
 }
+
+interface Window {
+  alt: Alt;
+}
+
+declare var alt: Alt;
