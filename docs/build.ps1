12da2da093e33afc846c30153b9dab2a6ef733c8
Martin <7252614+Lhoerion@users.noreply.github.com>
2020-10-26 01:59:38 +0100

    chore: Cleanup build tools

--- docs/build.ps1
+++ docs/build.ps1
@@ -1,21 +1,20 @@
 param(
-    [Switch] $disableChecks = $false,
     [string] $port = "8080",
-    [Switch] $clearCache = $false
+    [Switch] $cleanOnly = $false
 )
 
-function Cleanup() {
+function PostCleanup() {
     Remove-Item -Path 'docfx.zip' -Force 2>&1 > $null
     Remove-Item -Path 'docfx-plugins-typescriptreference.zip' -Force 2>&1 > $null
     Remove-Item -Path 'docfx-tmpls-discordfx.zip' -Force 2>&1 > $null
     Remove-Item -Path 'package.json' -Force 2>&1 > $null
     Remove-Item -Path 'package-lock.json' -Force 2>&1 > $null
-    Remove-Item -Path 'node_modules' -Recurse -Force 2>&1 > $null
-    if($clearCache) {
-        Remove-Item -Path '_site' -Recurse -Force 2>&1 > $null
-        Remove-Item -Path 'obj' -Recurse -Force 2>&1 > $null
-        Remove-Item -Path 'api/**.yml' -Force 2>&1 > $null
-        Remove-Item -Path 'api/.manifest' -Force 2>&1 > $null
+    Remove-Item -Path './node_modules/' -Recurse -Force 2>&1 > $null
+    if($cleanOnly) {
+        Remove-Item -Path './_site/' -Recurse -Force 2>&1 > $null
+        Remove-Item -Path './obj' -Recurse -Force 2>&1 > $null
+        Remove-Item -Path './api/**.yml' -Force 2>&1 > $null
+        Remove-Item -Path 'output.json' -Force 2>&1 > $null
     }
 }
 
@@ -80,38 +79,35 @@ function LogWrap([string] $msg, [ScriptBlock] $action, [boolean] $disResult=$fal
     }
 }
 
-if($clearCache) {
-    Cleanup
-    exit
-}
-
 try
 {
+    if($cleanOnly) { exit }
+
     LogWrap "Downloading DocFx package" {
         if(Test-Path "./docfx/docfx.exe") { return -0x1 }
         FetchAndDownloadRelease "dotnet/docfx" "docfx.zip" 2>$null
     }
     LogWrap "Extracting DocFx package" {
         if(Test-Path "./docfx/docfx.exe") { return -0x1 }
-        ExtractArchive "docfx.zip" 2>$null
+        ExtractArchive "docfx.zip" "./docfx/" 2>$null
     }
 
     LogWrap "Downloading DocFx TypeScriptReference package" {
-        if(Test-Path "./templates/docfx-plugins-typescriptreference") { return -0x1 }
+        if(Test-Path "./templates/docfx-plugins-typescriptreference/") { return -0x1 }
         FetchAndDownloadRelease "Lhoerion/DocFx.Plugins.TypeScriptReference" "docfx-plugins-typescriptreference.zip" 2>&1 6>$null
     }
     LogWrap "Extracting DocFx TypeScriptReference package" {
-        if(Test-Path "./templates/docfx-plugins-typescriptreference") { return -0x1 }
-        ExtractArchive "docfx-plugins-typescriptreference.zip" "templates/" 2>&1 6>$null
+        if(Test-Path "./templates/docfx-plugins-typescriptreference/") { return -0x1 }
+        ExtractArchive "docfx-plugins-typescriptreference.zip" "./templates/" 2>&1 6>$null
     }
 
     LogWrap "Downloading DocFx DiscordFX package" {
-        if(Test-Path "./templates/discordfx") { return -0x1 }
+        if(Test-Path "./templates/discordfx/") { return -0x1 }
         FetchAndDownloadRelease "Lhoerion/DiscordFX" "docfx-tmpls-discordfx.zip" 2>&1 6>$null
     }
     LogWrap "Extracting DocFx DiscordFX package" {
-        if(Test-Path "./templates/discordfx") { return -0x1 }
-        ExtractArchive "docfx-tmpls-discordfx.zip" "templates/" 2>&1 6>$null
+        if(Test-Path "./templates/discordfx/") { return -0x1 }
+        ExtractArchive "docfx-tmpls-discordfx.zip" "./templates/" 2>&1 6>$null
     }
 
     LogWrap "Installing node dependencies" {
@@ -142,7 +138,7 @@ try
     LogWrap "Generating project metadata" {
         $stderr=npx typedoc --options './typedoc.json' 2>$null
         if($LastExitCode -gt 0x0) { return $LastExitCode, $stderr }
-        $stderr=npx type2docfx './output.json' './api' --basePath '.' --sourceUrl 'https://github.com/altmp/altv-types' --sourceBranch 'master' --disableAlphabetOrder 2>&1 6>$null
+        $stderr=npx type2docfx './output.json' './api/' --basePath '.' --sourceUrl 'https://github.com/altmp/altv-types' --sourceBranch 'master' --disableAlphabetOrder 2>&1 6>$null
         return $LastExitCode, $buff
     }
 
@@ -150,5 +146,5 @@ try
 }
 finally
 {
-    Cleanup
+    PostCleanup
 }
