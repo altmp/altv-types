34775025b5d3f73b35685c8232fb4178e20fca6d
Blue <florian.uaz@web.de>
2021-01-25 14:59:32 +0100

    Fix example

--- client/index.d.ts
+++ client/index.d.ts
@@ -1904,7 +1904,8 @@ declare module "alt-client" {
    * Load a specific ytyp file.
    *
    * @param path Relative path to the game folder.
-   * @remarks Example: alt.loadYtyp("x64u.rpf/levels/gta5/_hills/country_06/country_06_metadata.rpf/cs6_08_interior_cs6_08_mine_int.ytyp")
+   * @example
+   * alt.loadYtyp("x64u.rpf/levels/gta5/_hills/country_06/country_06_metadata.rpf/cs6_08_interior_cs6_08_mine_int.ytyp")
    *
    * @alpha
    */
@@ -1914,7 +1915,8 @@ declare module "alt-client" {
    * Unload a specific ytyp file.
    *
    * @param path Relative path to the game folder.
-   * @remarks Example: alt.unloadYtyp("x64u.rpf/levels/gta5/_hills/country_06/country_06_metadata.rpf/cs6_08_interior_cs6_08_mine_int.ytyp")
+   * @example
+   * alt.unloadYtyp("x64u.rpf/levels/gta5/_hills/country_06/country_06_metadata.rpf/cs6_08_interior_cs6_08_mine_int.ytyp")
    *
    * @alpha
    */
