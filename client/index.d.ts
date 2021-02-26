681de7fe3e6a357438ed594d036a8ca4ec29d8e2
Blue <florian.uaz@web.de>
2021-02-27 00:16:43 +0100

    Thx leon

--- client/index.d.ts
+++ client/index.d.ts
@@ -757,14 +757,14 @@ declare module "alt-client" {
     /**
      * Set & get the volume for 3D Voice
      * 
-     * @remarks Value needs to be between 0-1
+     * @remarks Value needs to be between 0-1.
      */
     public spatialVolume: number
 
     /**
      * Set & get the volume for 2D Voice
      * 
-     * @remarks Value needs to be between 0-1
+     * @remarks Value needs to be between 0-1.
      */
     public nonSpatialVolume: number
   }
