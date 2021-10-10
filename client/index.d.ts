3dcbb0fc089858b8fd02708bcad6ed7d994a67ee
Blue <florian.uaz@web.de>
2021-10-10 16:12:57 +0200

    server|client: fix resourceError error param type

--- client/index.d.ts
+++ client/index.d.ts
@@ -185,7 +185,7 @@ declare module "alt-client" {
     removeEntity: (object: Entity) => void;
     resourceStart: (errored: boolean) => void;
     resourceStop: () => void;
-    resourceError: (error: string, file: string, line: number) => void;
+    resourceError: (error: Error, file: string, line: number) => void;
     syncedMetaChange: (entity: Entity, key: string, value: any, oldValue: any) => void;
     streamSyncedMetaChange: (entity: Entity, key: string, value: any, oldValue: any) => void;
     globalMetaChange: (key: string, value: any, oldValue: any) => void;
