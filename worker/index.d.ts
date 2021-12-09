5c6f1627a6e9cdcec2dcb1340cb0a89bba05b0ca
xshady <54737754+xxshady@users.noreply.github.com>
2021-12-09 14:42:40 +0300

    worker: Separate event functions

--- worker/index.d.ts
+++ worker/index.d.ts
@@ -11,6 +11,41 @@ declare module "alt-worker" {
    * @returns The shared array buffer instance.
    */
   export function getSharedArrayBuffer(id: number): SharedArrayBuffer;
+  
+  /**
+   * Emits specified event across particular client.
+   *
+   * @param eventName Name of the event.
+   * @param args Rest parameters for emit to send.
+   */
+  export function emit(eventName: string, ...args: any[]): void;
 
-  export { on, once, emit, log, logWarning, logError, setTimeout, setInterval, nextTick, clearTimeout, clearInterval, clearNextTick, hash, version, branch, sdkVersion, debug, File, RGBA, Vector3, Vector2 } from "alt-client";
+  /**
+   * Unsubscribes from client event handler with specified listener.
+   *
+   * @remarks Listener should be of the same reference as when event was subscribed.
+   *
+   * @param eventName Name of the event.
+   * @param listener Listener that should be removed.
+   */
+  export function off(eventName: string, listener: (...args: any[]) => void): void;
+
+  /**
+   * Subscribes to client event handler with specified listener.
+   *
+   * @param eventName Name of the event.
+   * @param listener Listener that should be added.
+   */
+  export function on(eventName: string, listener: (...args: any[]) => void): void;
+  
+  
+  /**
+   * Subscribes to a custom client event with the specified listener, which only triggers once.
+   *
+   * @param eventName Name of the event.
+   * @param listener Listener that should be added.
+   */
+  export function once(eventName: string, listener: (...args: any[]) => void): void;
+
+  export { log, logWarning, logError, setTimeout, setInterval, nextTick, clearTimeout, clearInterval, clearNextTick, hash, version, branch, sdkVersion, debug, File, RGBA, Vector3, Vector2 } from "alt-client";
 }
