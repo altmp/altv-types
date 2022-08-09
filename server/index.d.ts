23ea3456665e361cc824bf4068e0d7beb5e2f4b4
xshady <fastik036@gmail.com>
2022-08-09 19:47:06 +0300

    shared|server|client: Move CheckpointType enum to shared

--- server/index.d.ts
+++ server/index.d.ts
@@ -103,76 +103,6 @@ declare module "alt-server" {
     CheckpointCylinder,
   }
 
-  export const enum CheckpointType {
-    CylinderSingleArrow,
-    CylinderDoubleArrow,
-    CylinderTripleArrow,
-    CylinderCycleArrow,
-    CylinderCheckerboard,
-    CylinderWrench,
-    CylinderSingleArrow2,
-    CylinderDoubleArrow2,
-    CylinderTripleArrow2,
-    CylinderCycleArrow2,
-    CylinderCheckerboard2,
-    CylinderWrench2,
-    RingSingleArrow,
-    RingDoubleArrow,
-    RingTripleArrow,
-    RingCycleArrow,
-    RingCheckerboard,
-    SingleArrow,
-    DoubleArrow,
-    TripleArrow,
-    CycleArrow,
-    Checkerboard,
-    CylinderSingleArrow3,
-    CylinderDoubleArrow3,
-    CylinderTripleArrow3,
-    CylinderCycleArrow3,
-    CylinderCheckerboard3,
-    CylinderSingleArrow4,
-    CylinderDoubleArrow4,
-    CylinderTripleArrow4,
-    CylinderCycleArrow4,
-    CylinderCheckerboard4,
-    CylinderSingleArrow5,
-    CylinderDoubleArrow5,
-    CylinderTripleArrow5,
-    CylinderCycleArrow5,
-    CylinderCheckerboard5,
-    RingPlaneUp,
-    RingPlaneLeft,
-    RingPlaneRight,
-    RingPlaneDown,
-    Empty,
-    Ring,
-    Empty2,
-    Cylinder,
-    Cylinder1,
-    Cylinder2,
-    Cylinder3,
-    Cylinder4,
-    Cylinder5,
-    Empty3,
-    Empty4,
-    Empty5,
-    Empty6,
-    RingDollar,
-    RingWolf,
-    RingQuestionMark,
-    RingPlane,
-    RingChopper,
-    RingBoat,
-    RingCar,
-    RingBike,
-    RingBicycle,
-    RingTruck,
-    RingParachute,
-    RingJetpack,
-    RingWhirl,
-  }
-
   export const enum RadioStation {
     LosSantosRockRadio,
     NonStopPopFm,
@@ -2442,8 +2372,8 @@ declare module "alt-server" {
   }
 
   export class Checkpoint extends Colshape {
-    constructor(type: number, x: number, y: number, z: number, radius: number, height: number, r: number, g: number, b: number, a: number);
-    constructor(type: number, pos: shared.IVector3, radius: number, height: number, color: shared.RGBA);
+    constructor(type: shared.CheckpointType, x: number, y: number, z: number, radius: number, height: number, r: number, g: number, b: number, a: number);
+    constructor(type: shared.CheckpointType, pos: shared.IVector3, radius: number, height: number, color: shared.RGBA);
 
     public deleteMeta(key: string): void;
     public deleteMeta<K extends shared.ExtractStringKeys<ICustomCheckpointMeta>>(key: K): void;
