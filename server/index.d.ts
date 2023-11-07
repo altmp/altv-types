03d2aaf64362c222c809c66152a1b4782a2d520c
xLuxy <67131061+xLuxy@users.noreply.github.com>
2023-11-07 16:45:16 +0100

    server: Add Player.removeHeadBlendPaletteColor

--- server/index.d.ts
+++ server/index.d.ts
@@ -1469,6 +1469,8 @@ declare module "alt-server" {
 
     public getHeadBlendPaletteColor(id: number): shared.RGBA;
 
+    public removeHeadBlendPaletteColor(): void;
+
     public setHeadBlendData(shapeFirstID: number, shapeSecondID: number, shapeThirdID: number, skinFirstID: number, skinSecondID: number, skinThirdID: number, shapeMix: number, skinMix: number, thirdMix: number): void;
 
     public getHeadBlendData(): IHeadBlendData;
