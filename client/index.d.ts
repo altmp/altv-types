524d9df08a737f3e31177e2c7489501891de0a1d
xshady <54737754+xxshady@users.noreply.github.com>
2023-12-06 23:53:49 +0300

    fix(client): LocalVehicle class (#292)

--- client/index.d.ts
+++ client/index.d.ts
@@ -980,7 +980,7 @@ declare module "alt-client" {
     public readonly name: string;
 
     /** Player's vehicle, null if player is not in any vehicle */
-    public readonly vehicle: Vehicle | LocalVehicle | null;
+    public readonly vehicle: Vehicle | null;
 
     public readonly isDead: boolean;
 
@@ -4112,7 +4112,7 @@ declare module "alt-client" {
     public faceCamera: boolean;
   }
 
-  export class LocalVehicle extends WorldObject {
+  export class LocalVehicle extends Vehicle {
     public constructor(model: string | number, dimension: number, pos: shared.IVector3, rot: shared.IVector3, useStreaming?: boolean, streamingDistance?: number);
 
     /**
